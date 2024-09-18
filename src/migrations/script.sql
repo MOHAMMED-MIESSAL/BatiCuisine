923646f002f5f14d1e2cd67c95407ec11142edd5
Mohammed Miessal <mohammedmiessal@gmail.com>
2024-09-18 19:20:22 +0100

    Create a Scipt SQL for migrations

---
+++ src/migrations/script.sql
@@ -0,0 +1,57 @@
+-- ENUM for Project Status :
+CREATE TYPE project_status AS ENUM ('En cours', 'Terminé', 'Annulé');
+
+
+-- Table client
+CREATE TABLE client (
+                        id               SERIAL PRIMARY KEY,
+                        name             VARCHAR(50) NOT NULL,
+                        address          VARCHAR(50),
+                        phone            VARCHAR(20),
+                        is_professional  BOOLEAN NOT NULL
+);
+
+-- Table project
+CREATE TABLE project (
+                         id             SERIAL PRIMARY KEY,
+                         name           VARCHAR(50) NOT NULL,
+                         profit_margin  NUMERIC(10, 2),
+                         total_cost     NUMERIC(10, 2),
+                         state_project  project_status,
+                         client_id      INTEGER REFERENCES client (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Table component
+CREATE TABLE component (
+                           id              SERIAL PRIMARY KEY,
+                           name            VARCHAR(50) NOT NULL,
+                           type_component  VARCHAR(20) NOT NULL, -- Material || manPower
+                           vat_rate        NUMERIC(5, 2)
+);
+
+-- Table material
+CREATE TABLE material (
+                          id                 SERIAL PRIMARY KEY,
+                          unit_cost          NUMERIC(10, 2),
+                          quantity           NUMERIC(10, 2) CHECK (quantity >= 0),
+                          transport_cost     NUMERIC(10, 2),
+                          coefficient_quality NUMERIC(5, 2)
+) INHERITS (component);
+
+-- Table manPower
+CREATE TABLE man_power (
+                           id                  SERIAL PRIMARY KEY,
+                           hourly_rate         NUMERIC(10, 2),
+                           hours_work          NUMERIC(10, 2),
+                           worker_productivity  NUMERIC(5, 2)
+) INHERITS (component);
+
+-- Table quotes
+CREATE TABLE estimate (
+                          id              SERIAL PRIMARY KEY,
+                          amount_estimate NUMERIC(10, 2),
+                          date_emission   DATE,
+                          date_validity   DATE,
+                          accepted        BOOLEAN,
+                          project_id      INTEGER REFERENCES project(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
